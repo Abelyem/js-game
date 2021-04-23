0e09213783c6fe86fe99bdb827305efd9be7dd44
Abelyem <abel.yemane1@hotmail.co.uk>
2021-04-23 18:04:12 +0000

    added movement to dino / added new img for dino (red square now running man / created if statement to allow dino to jump over obstacles / added if statement for game to end if dino collides with obstacle / added random obstacle generator via math.random / added game over (plus all obstacles to disappear) upon collision with dino and obstacle

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+
     const dino = document.querySelector('.dino')
+    const grid = document.querySelector('.grid')
+    const alert = document.getElementById('alert')
+
     let isJumping = false
     let gravity = 0.9
+    let isGameOver = false
 
     function control(e) {
         if (e.keyCode === 32) {
@@ -19,29 +24,68 @@ document.addEventListener('DOMContentLoaded', () => {
     let position = 0
 
     function jump() {
+        count = 0
         let timerId = setInterval(function () {
 
             // move down
-            if (position === 150) {
+            if (count === 15) {
                 clearInterval(timerId)
                 console.log('down')
                 let downTimerId = setInterval(function () {
-                    if (position === 0) {
+                    if (count === 0) {
                         clearInterval(downTimerId)
                         isJumping = false
                     }
-                    position -= 30
+                    position -= 5
+                    count--
+                    position = position * gravity
                     dino.style.bottom = position + 'px'
                 }, 20)
             }
-
-
             //move up
             console.log('up')
             position += 30
+            count++
+            position = position * gravity
             dino.style.bottom = position + 'px'
+            console.log(dino.style.bottom)
         }, 20)
     }
+
+
+    function generateObstacles() {
+        let randomTime = Math.random() * 4000
+
+        let obstaclePosition = 1000
+        const obstacle = document.createElement('div')
+        if (!isGameOver) obstacle.classList.add('obstacle')
+        grid.appendChild(obstacle)
+        obstacle.style.left = obstaclePosition + 'px'
+
+        let timerId = setInterval(function () {
+            if (obstaclePosition > 0 && obstaclePosition < 60 && position < 60) {
+                clearInterval(timerId)
+                alert.innerHTML = 'Game Over'
+                isGameOver = true
+
+                // removes all children divs
+                while (grid.firstChild) { // if this is true
+
+                    // if dino hits 1st child, all div (obstacles) will also be removed
+                    grid.removeChild(grid.lastChild)
+                }
+
+
+            }
+
+            obstaclePosition -= 10
+            obstacle.style.left = obstaclePosition + 'px'
+        }, 20)
+
+        if (!isGameOver) setTimeout(generateObstacles, randomTime)
+
+    }
+    generateObstacles()
 
 
 })
